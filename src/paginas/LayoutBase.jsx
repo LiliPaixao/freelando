53c5d9984d8fe534d29394002f75353d1e86d2f4
Liliane <liliane-paixao@ielsc.org.br>
2025-06-29 18:01:25 -0300

    Incluindo cabeçalho e rodapé como layout base para todas as páginas.

---
+++ src/paginas/LayoutBase.jsx
@@ -0,0 +1,64 @@
+import { Row, Col, Container } from "react-grid-system";
+import { Cabecalho } from "../componentes/Cabecalho/Cabecalho";
+import { FreelandoLogo } from "../componentes/Icones/FreelandoLogo";
+import { Link } from "../componentes/Link/Link";
+import { Rodape } from "../componentes/Rodape/Rodape";
+import { Tipografia } from "../componentes/Tipografia/Tipografia";
+import { ListaInline } from "../componentes/Lista/ListaInline";
+import { ItemListaInline } from "../componentes/Lista/ItemListaInline";
+import { IconeWhatsApp } from "../componentes/Icones/IconeWhatsApp";
+import { IconeTwitch } from "../componentes/Icones/IconeTwitch";
+import { IconeInstagram } from "../componentes/Icones/IconeInstagram";
+import { IconeTwitter } from "../componentes/Icones/IconeTwitter";
+import { Outlet } from "react-router-dom";
+
+const LayoutBase = () => {
+    return (
+        <>
+            <Cabecalho>
+                <Container>
+                    <Row align="center">
+                        <Col>
+                            <FreelandoLogo />
+                        </Col>
+                        <Col style={{ textAlign: 'right' }}>
+                            <Link>Login</Link>
+                        </Col>
+                    </Row>
+                </Container>
+            </Cabecalho>
+            < Outlet />
+            <Rodape>
+                <Container>
+                    <Row align="center">
+                        <Col>
+                            <FreelandoLogo height={40} width={176} />
+                            <Tipografia variante="legenda" componente="legenda">
+                                Desenvolvido por Alura. Projeto fictício sem fins comerciais.
+                            </Tipografia>
+                        </Col>
+                        <Col style={{ textAlign: 'right' }}>
+                            <Tipografia variante="legenda" componente="legenda">Acesse nossas redes:</Tipografia>
+                            <ListaInline>
+                                <ItemListaInline>
+                                    <a href="/" aria-label="Link para WhatsApp"><IconeWhatsApp /></a>
+                                </ItemListaInline>
+                                <ItemListaInline>
+                                    <a href="/" aria-label="Link para a Twitch"><IconeTwitch /></a>
+                                </ItemListaInline>
+                                <ItemListaInline>
+                                    <a href="/" aria-label="Link para o Instagram"><IconeInstagram /></a>
+                                </ItemListaInline>
+                                <ItemListaInline>
+                                    <a href="/" aria-label="Link para o Twitter"><IconeTwitter /></a>
+                                </ItemListaInline>
+                            </ListaInline>
+                        </Col>
+                    </Row>
+                </Container>
+            </Rodape>
+        </>
+    );
+}
+
+export default LayoutBase;
